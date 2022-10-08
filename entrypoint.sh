f82d57a0fdb465ea763f8e10b1257def80a0bc87
gucao <280780363@qq.com>
2022-10-08 13:39:23 +0800

    ....

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 cd /docker-mongo
+verion=$INPUT_VERSION
+username=$INPUT_USERNAME
+password=INPUT_PASSWORD
 
-build="docker build -t mongodb/snc --build-arg VERSION=$INPUT_VERSION . "
+build="docker build -t mongodb/snc --build-arg VERSION=$version . "
 sh -c "$build"
-echo "image build finished, version: $INPUT_VERSION"
+echo "image build finished, version: $version"
 
-# run="docker run --name mongo -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=$INPUT_USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$INPUT_PASSWORD mongodb/snc mongod --replSet rs0 --keyFile /mongodb.key --bind_ip_all"
-# sh -c "$run"
-# echo "mongodb is running username: $INPUT_USERNAME, password: $INPUT_PASSWORD"
+run="docker run --name mongo -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=$username -e MONGO_INITDB_ROOT_PASSWORD=$password mongodb/snc mongod --replSet rs0 --keyFile /mongodb.key --bind_ip_all"
+sh -c "$run"
+echo "mongodb is running username: $username, password: $password"
 
-# sleep 20s
+sleep 20s
 
-# rs='docker exec mongo sh -c "mongosh -u $INPUT_USERNAME, -p $INPUT_PASSWORD < /rs.js"'
-# sh -c "$rs"
-# echo "rs initiate finished"
+rs='docker exec mongo sh -c "mongosh -u $n, -p $password < /rs.js"'
+sh -c "$rs"
+echo "rs initiate finished"
