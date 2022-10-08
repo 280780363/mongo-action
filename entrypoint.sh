e95b2213115e189c9b35b5539f387d266d574404
gucao <280780363@qq.com>
2022-10-08 11:56:31 +0800

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /docker-mongo
 
-docker build -t mongodb/snc --build-arg VERSION=$VERSION . 
+docker build -t mongodb/snc --build-arg VERSION=latest . 
 
 docker run --name mongo -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=$USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$PASSWORD mongodb/snc mongod --replSet rs0 --keyFile /mongodb.key --bind_ip_all
 
