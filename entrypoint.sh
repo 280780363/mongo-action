fb6bd6be95d496d7eafd73b5d71af13521772c16
gucao <280780363@qq.com>
2022-10-08 12:07:07 +0800

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,18 @@
 
 cd /docker-mongo
 
-docker build -t mongodb/snc --build-arg VERSION=${VERSION} . 
-
+build="docker build -t mongodb/snc --build-arg VERSION=$VERSION . "
+sh -c "$build"
 echo 'image build finished'
 
-docker run --name mongo -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=${USERNAME} -e MONGO_INITDB_ROOT_PASSWORD=${PASSWORD} mongodb/snc mongod --replSet rs0 --keyFile /mongodb.key --bind_ip_all
-
+run="docker run --name mongo -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=$USERNAME -e MONGO_INITDB_ROOT_PASSWORD=$PASSWORD mongodb/snc mongod --replSet rs0 --keyFile /mongodb.key --bind_ip_all"
+sh -c "$run"
 echo 'mongodb is running'
 
 sleep 20s
 
-docker exec mongo sh -c "mongosh -u ${USERNAME} -p ${PASSWORD} < /rs.js"
-
+rs='docker exec mongo sh -c "mongosh -u $USERNAME -p $PASSWORD < /rs.js"'
+sh -c "$rs"
 echo 'rs initiate finished'
 
 
